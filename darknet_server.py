8adbc2cb1bc5897135789ef0235280b38573cd8c
yu.omori <yu.omori0521@gmai.com>
2018-08-11 19:02:30 +0900

    little modify

--- darknet_server.py
+++ darknet_server.py
@@ -1,7 +1,5 @@
-#!/usr/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-from __future__ import division, print_function, absolute_import, unicode_literals
 
 import argparse
 import datetime
